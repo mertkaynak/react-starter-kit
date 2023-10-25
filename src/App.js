c5afce9424973b7b9f3bef742cc19abd9c35fbe2
mertkaynak <kaynak.mert35@gmail.com>
2023-10-26 02:10:12 +0300

    message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      test
+      <img src="logo192.png"/>
+      <img src={logo}/>
+    </div>
+  );
+}
+
+export default App;
