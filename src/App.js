8485d4d377572af462d74bd518d3b6dceff21488
mertkaynak <kaynak.mert35@gmail.com>
2023-10-26 02:43:47 +0300

    ortam değişkenleri

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
 import logo from './logo.svg';
 
 function App() {
+
+  useEffect(() => {
+    if(process.env.NODE_ENV === "production"){
+      // analytics'i bilgilendir.
+    }
+  })
   return (
     <div className="App">
-      test
-      <img src="logo192.png"/>
-      <img src={logo}/>
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>
+        {process.env.REACT_APP_API_URL}
+      </p>
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <img src="logo192.png" />
+          <img src={logo} />
+        </>
+      )}
     </div>
   );
 }
